0cae9e7e973e7aa35e725d388343cf5af4398a0e
Tyuiu.KalimullinaAH@outlook.com <boulevarovaalina@icloud.com>
2023-09-06 15:43:12 +0500

    Task2

---
+++ Tyuiu.KalimullinaAH.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KalimullinaAH.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KalimullinaAH.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        { 
+        
+            //область создания методов тестирования, методов из библиотеки
+            var name = "Алина";
+            var res = DataService.GetMessage(name);
+            // Вызов класса Assert и метод AreEqual
+            Assert.AreEqual("Привет,Алина",res);
+        }
+    }
+}
